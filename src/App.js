36b37add0358582cd3a824d6b25610a8c0ade063
ipjessica <ipjessica9@gmail.com>
2021-09-29 15:48:49 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import Home from "./screens/Home"
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import HomeScreen from './screens/HomeScreen';
+import AssetScreen from './screens/AssetScreen';
+import GalleriesScreen from './screens/GalleriesScreen';
+import GalleryDetailScreen from './screens/GalleryDetailScreen';
+
 import Galleries from "./screens/Galleries";
 import Assets from "./screens/Assets";
 import AssetDetail from "./screens/Asset-detail";
@@ -10,33 +15,31 @@ import Signin from "./screens/Signin";
 import Signup from "./screens/Signup";
 import GalleryDetail from "./screens/Gallery-detail";
 
-
-
 function App() {
   return (
-      <Router>
-          <Header/>
-          <main className="py-3">
-              <Container>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+    
+            <Route path='/' component={ HomeScreen } exact />
+            <Route path='/signin' component={ Signin } />
+            <Route path='/signup' component={ Signup } />
 
-                  <Route path='/' component={ Home } exact/>
-                  <Route path='/signin' component={ Signin } exact/>
-                  <Route path='/signup' component={ Signup } exact/>
+            <Route path='/galleries' component={ GalleriesScreen } />
+            <Route path='/artists' component={ Galleries }/>
+            <Route path='/asset/:id' component={ AssetScreen } />
 
-                  <Route path='/galleries' component={ Galleries }/>
-                  <Route path='/artists' component={ Galleries }/>
-                  <Route path='/assets/:id' component={ AssetDetail } exact/>
+            <Route path='/gallery/:slug' component={ GalleryDetailScreen } />
+            <Route path='/artist/:slug' component={ AssetDetail } />
 
-                  <Route path='/gallery/:slug' component={ GalleryDetail } exact/>
-                  <Route path='/artist/:slug' component={ AssetDetail } exact/>
-
-                  <Route path=':user/assets' component={ AssetDetail } exact/>
-                  <Route path=':user/assets/create' component={ AssetDetail } exact/>
-
-              </Container>
-          </main>
-          <Footer/>
-      </Router>
+            <Route path=':user/assets' component={ AssetDetail } />
+            <Route path=':user/assets/create' component={ AssetDetail } />
+                 
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
