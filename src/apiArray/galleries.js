333b3434af9bbfa43809e3fc83a63ed8991cba9a
ipjessica <ipjessica9@gmail.com>
2021-09-29 15:54:42 -0400

    Create galleries.js

---
+++ src/apiArray/galleries.js
@@ -0,0 +1,37 @@
+const galleries = [
+  {
+    "id":1,
+    "name":"Starship",
+    "slug":"starship",
+    "imglogo":"https://artyste-demo.s3.amazonaws.com/galleries/1/1_1628579097.jpg?AWSAccessKeyId=AKIAQJ4TKPUZGUAQXN5D&Signature=E9K9EhfPti%2Be2ZO9Xdc1ABuO2Bc%3D&Expires=1632882786",
+    "imgbanner":"https://artyste-demo.s3.amazonaws.com/galleries/1/1_1628579098.jpg?AWSAccessKeyId=AKIAQJ4TKPUZGUAQXN5D&Signature=l2%2F7UkBxVn4OheWEOXcofYxfLok%3D&Expires=1632882786",
+    "admin":1
+  },
+  {
+    "id":2,
+    "name":"Louvre",
+    "slug":"louvre",
+    "imglogo":"https://artyste-demo.s3.amazonaws.com/galleries/None/None_1628579434.jpg?AWSAccessKeyId=AKIAQJ4TKPUZGUAQXN5D&Signature=AQBPYXBvChqi5T3I3cqGxjFiLLk%3D&Expires=1632882786",
+    "imgbanner":"https://artyste-demo.s3.amazonaws.com/galleries/None/None_1628579435.jpg?AWSAccessKeyId=AKIAQJ4TKPUZGUAQXN5D&Signature=f%2Fz1imrujUOubbPM%2B1kOmdlSZxw%3D&Expires=1632882786",
+    "admin":3
+  },
+  {
+    "id":3,
+    "name":"Penguin",
+    "slug":"penguin",
+    "imglogo":"https://artyste-demo.s3.amazonaws.com/galleries/3/3_1628877150.jpg?AWSAccessKeyId=AKIAQJ4TKPUZGUAQXN5D&Signature=YBSy1v%2FWnCMGNPumOIVwz8CMF1o%3D&Expires=1632882786",
+    "imgbanner":"https://artyste-demo.s3.amazonaws.com/galleries/3/3_1629848218.jpg?AWSAccessKeyId=AKIAQJ4TKPUZGUAQXN5D&Signature=v5%2F6sYO5t1UBL7S6xWAKlY7C57I%3D&Expires=1632882786",
+    "admin":3
+  },
+  {
+    "id":4,
+    "name":"The Art Institute of Chicago",
+    "slug":"the-art-Institute-of-chicago",
+    "imglogo":"https://artyste-demo.s3.amazonaws.com/galleries/None/None_1628837237.jpg?AWSAccessKeyId=AKIAQJ4TKPUZGUAQXN5D&Signature=5DUdnwdw%2B0RQbwrvz4Sc%2FxfYCRA%3D&Expires=1632882786",
+    "imgbanner":"https://artyste-demo.s3.amazonaws.com/galleries/4/4_1628837906.jpg?AWSAccessKeyId=AKIAQJ4TKPUZGUAQXN5D&Signature=o1CBxjqnhYGEjufGfBPQSuZInEI%3D&Expires=1632882786",
+    "admin":3
+  }
+
+]
+
+export default galleries
