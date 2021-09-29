352fa30c9a66848281fa1eaef65dcf6fdbce6b89
ipjessica <ipjessica9@gmail.com>
2021-09-29 16:01:06 -0400

    Update Gallery-detail.js

--- src/screens/Gallery-detail.js
+++ src/screens/Gallery-detail.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
+import Gallery from '../components/Gallery';
+import galleries from '../apiArray/galleries';
+import louvre from '../apiArray/louvre';
+import GalleriesScreen from './GalleriesScreen';
 
-function GalleryDetail() {
+
+function GalleryDetailScreen({ match }) {
+    const galleryDetail = galleries.find((g) => g.id === match.params.id)
     return (
-        <div>
-            <h3>Gallery Detail</h3>
-            <hr/>
+        <Container>
+            <Link to='/galleries' className='btn btn-light my-3'>Go Back</Link>
+            <Image src={galleries.imgbanner} alt={galleries.name} fluid />
+            <Row>
+                <Col>
 
-            <p><a href="http://app.arthology.io/gallery/starship" target="_blank">http://app.arthology.io/gallery/:slug</a></p>
+                </Col>
+            </Row>
+                
+            <div>
+                <p><a href="http://app.arthology.io/gallery/starship" target="_blank">http://app.arthology.io/gallery/:slug</a></p>
 
-            <p><a href="https://github.com/artyste/artyste-backend-demo/blob/master/art/templates/art/gallery.html" target="_blank">https://github.com/artyste/artyste-backend-demo/blob/master/art/templates/art/gallery.html</a></p>
+                <p><a href="https://github.com/artyste/artyste-backend-demo/blob/master/art/templates/art/gallery.html" target="_blank">https://github.com/artyste/artyste-backend-demo/blob/master/art/templates/art/gallery.html</a></p>
 
-            <p>Api End-point<br/>
-            <a href="https://api.artyste.info/v1/gallery/starship/" target="_blank">https://api.artyste.info/v1/gallery/starship/</a>
-            </p>
-        </div>
-    );
+                <p>Api End-point<br/>
+                <a href="https://api.artyste.info/v1/gallery/starship/" target="_blank">https://api.artyste.info/v1/gallery/starship/</a>
+                </p>
+            </div>
+        </Container>
+    )
 }
 
-export default GalleryDetail;
+export default GalleryDetailScreen
