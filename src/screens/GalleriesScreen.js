39d964b80eafb4d52cb74c34f5811e783e1e0096
ipjessica <ipjessica9@gmail.com>
2021-09-29 16:02:50 -0400

    Update and rename Galleries.js to GalleriesScreen.js

--- src/screens/GalleriesScreen.js
+++ src/screens/GalleriesScreen.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
+import galleries from '../apiArray/galleries';
+import Gallery from '../components/Gallery';
 
-function Galleries() {
+function GalleriesScreen({ match }) {
+    const gallery = galleries.find((g) => g.id === match.params.id)
     return (
         <div>
-            <h3>Galleries</h3>
+            <h1>Galleries</h1>
             <hr/>
+            <Row>
+                {galleries.map(gallery => (
+                    <Col key={gallery.id} sm={12} md={6} lg={4} xl={3}>
+                        <Gallery gallery={gallery} />
+                    </Col>
+                ))}
+            </Row>
+        </div>
+        <div>
 
             <p><a href="https://api.artyste.info/galleries/" target="_blank">https://api.artyste.info/galleries/</a></p>
 
