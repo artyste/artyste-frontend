d5e5e8b476df4d47ffda262d2e843835371dc85d
ipjessica <ipjessica9@gmail.com>
2021-10-01 12:38:46 -0400

    Create galleryReducers.js

---
+++ src/reducers/galleryReducers.js
@@ -0,0 +1,25 @@
+import { 
+    GALLERY_LIST_REQUEST,
+    GALLERY_LIST_SUCCESS,
+    GALLERY_LIST_FAIL,
+
+    GALLERY_DETAILS_REQUEST,
+    GALLERY_DETAILS_SUCCESS,
+    GALLERY_DETAILS_FAIL,
+ } from '../constants/galleryConstants'; 
+
+export const galleryListReducer = (state = { galleries: [] }, action) => {
+    switch(action.type){
+        case GALLERY_LIST_REQUEST:
+            return { loading: true, galleries:[] }
+
+        case GALLERY_LIST_SUCCESS:
+            return { loading: false, galleries: action.payload }
+
+        case GALLERY_LIST_FAIL:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
