b22713b94bb52cda8b84c306c1aa1de0ea9bb935
ipjessica <ipjessica9@gmail.com>
2021-10-01 12:36:58 -0400

    Create assetActions.js

---
+++ src/actions/assetActions.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { 
+    ASSET_LIST_REQUEST,
+    ASSET_LIST_SUCCESS,
+    ASSET_LIST_FAIL,
+
+    ASSET_DETAILS_REQUEST,
+    ASSET_DETAILS_SUCCESS,
+    ASSET_DETAILS_FAIL,
+ } from '../constants/assetConstants'; 
+
+export const listAssets = () => async (dispatch) => {
+    try{
+        dispatch({ type: ASSET_LIST_REQUEST })
+
+        const { data } = await axios.get('https://api.artyste.info/v1/assets-list/')
+
+        dispatch({ 
+            type: ASSET_LIST_SUCCESS,
+            payload: data
+        })
+    }catch(error){
+        dispatch({ 
+            type: ASSET_LIST_FAIL,
+            payload: error.response && error.response.data.message
+              ? error.response.data.message
+              : error.message,
+        })
+    }
+
+}
+
+export const listAssetDetails = (id) => async (dispatch) => {
+    try{
+        dispatch({ type: ASSET_DETAILS_REQUEST })
+
+        // const { data } = await axios.get('https://api.artyste.info/v1/assets-list/')
+        const { data } = await axios.get(`https://api.artyste.info/v1/asset/${id}`)
+
+        dispatch({ 
+            type: ASSET_DETAILS_SUCCESS,
+            payload: data
+        })
+    }catch(error){
+        dispatch({ 
+            type: ASSET_DETAILS_FAIL,
+            payload: error.response && error.response.data.message
+              ? error.response.data.message
+              : error.message,
+        })
+    }
+
+}
