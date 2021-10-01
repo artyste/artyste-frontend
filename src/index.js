cb5dfb5875443e217ad13deb3a3b1b0da407e0b6
ipjessica <ipjessica9@gmail.com>
2021-10-01 12:55:59 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import store from './store'
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './static/css/index.css';
 import './static/css/bootstrap.min.css';
