0fb4900d7849d2571813234208a738816d7bee21
ipjessica <ipjessica9@gmail.com>
2021-10-01 12:38:20 -0400

    Create assetReducers.js

---
+++ src/reducers/assetReducers.js
@@ -0,0 +1,41 @@
+import { 
+    ASSET_LIST_REQUEST,
+    ASSET_LIST_SUCCESS,
+    ASSET_LIST_FAIL,
+
+    ASSET_DETAILS_REQUEST,
+    ASSET_DETAILS_SUCCESS,
+    ASSET_DETAILS_FAIL
+ } from '../constants/assetConstants'; 
+
+export const assetListReducer = (state = { assets: [] }, action) => {
+    switch(action.type){
+        case ASSET_LIST_REQUEST:
+            return { loading: true, assets:[] }
+
+        case ASSET_LIST_SUCCESS:
+            return { loading: false, assets: action.payload }
+
+        case ASSET_LIST_FAIL:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
+
+export const assetDetailsReducer = (state = { asset: [] }, action) => {
+    switch(action.type){
+        case ASSET_DETAILS_REQUEST:
+            return { loading: true, ...state }
+
+        case ASSET_DETAILS_SUCCESS:
+            return { loading: false, asset: action.payload }
+
+        case ASSET_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
